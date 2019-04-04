219167038463d6395f728045ee72b0c7d1ba10b5
ChrstphGrnr <ChristophGruener@me.com>
2019-04-04 17:08:21 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,31 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles/new' do 
+    erb :new
+  end
+
+  post '/articles' do 
+    # binding.pry
+    Article.create(params)
+    # binding.pry
+    redirect '/articles/:id'
+
+  end
+
+  get '/articles' do 
+    @articles = Article.all 
+    erb :index
+  end
+
+  get '/articles/:id' do 
+    # binding.pry
+    @article = Article.find_by(params)
+    erb :show
+  end
+
+
+
+
 end
