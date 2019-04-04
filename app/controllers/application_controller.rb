5198fd56e88f9e26f372ec68cd409a8930004515
BebopVinh <vinhdo07@gmail.com>
2019-04-03 20:49:12 -0700

    controller methods defined

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,34 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get 'articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    erb :index
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    erb :edit
+  end
+
+  patch "/articles/:id" do
+
+  end
+
+  delete '/articles/:id' do
+    
+  end
 end
