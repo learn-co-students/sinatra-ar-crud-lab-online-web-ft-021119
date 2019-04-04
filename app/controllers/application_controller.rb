b9346a3f357c2e786a4f92fde46afcc79aab2463
Taylor Swinton <taylorswinton1@gmail.com>
2019-04-04 19:04:19 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
-
 require_relative '../../config/environment'
+#require_relative 'models/article.rb'
+
 
 class ApplicationController < Sinatra::Base
 
@@ -8,6 +9,27 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/articles/new' do
+
+    erb :new
   end
+
+  post '/articles' do
+    @article = Article.create(params)
+
+    redirect "/articles/#{@article.id}"
+  end
+
+  get '/articles' do
+    @articles = Article.all
+
+    erb :index
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+
+    erb :show
+  end
+
 end
