28f43a48f2a29b52387bd87edc594f58add505c4
Lizzie <lchan217@gmail.com>
2019-04-04 09:02:04 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,23 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/articles/new' do
+    erb :new
   end
+
+  post '/articles' do
+      Article.create(params)
+      binding.pry
+      redirect "/articles"
+  end
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
 end
