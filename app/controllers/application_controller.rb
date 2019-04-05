ab3dc944c0d95cfab0ec08c0a9077fd11123a4f5
kirstenwerner <kirstenmwerner@gmail.com>
2019-04-05 16:06:25 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,49 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    # @articles = Article[:title]
+    # binding.pry
+    # erb :'index'
+  end
+
+  get '/articles/new' do
+    erb :'new'
+  end
+
+  post '/articles' do
+    @article = Article.new(params)
+    # redirect '/articles'
+    erb :'index'
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :'index'
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params["id"])
+    erb :'show'
+  end
+
+  get '/articles/:id/edit' do
+    erb :'edit'
+  end
+
+  patch '/articles/:id' do
+    id = params["id"]
+    new_params = {}
+    old_article = Post.find(id)
+    new_params[:title] = params["title"]
+    new_params[:content] = params["content"]
+    old_post.update(new_params)
+
+    redirect "/articles/#{id}"
+  end
+
+  delete '/articles/:id/delete' do
+    @article = Article.find(params["id"])
+    @particle.destroy
+    erb :delete
   end
 end
