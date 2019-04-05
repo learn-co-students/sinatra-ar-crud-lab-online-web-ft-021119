4d68aff7df1b3256e1d27bc8ab549dfc14bd4025
Burak Kocak <burakkocak884@ghotmail.com>
2019-04-05 15:04:46 -0400

    early start

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,26 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+  	@articles = Article.select(:title)
+  	erb :index
   end
-end
+
+  
+  get '/articles/new' do 
+  	erb :new
+
+
+   end
+   get '/arcticles' do 
+   	@article = Arcticle.new(params[:article])
+   	erb :show
+
+   end
+   get '/article/:id' do 
+   	@article = Article.find(param[:id])
+
+   end
+
+
+		
+	end
