76429c3447d53b0a8a8ae42271aff7348af41ad1
kirstenwerner <kirstenmwerner@gmail.com>
2019-04-05 17:20:38 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
-
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -9,49 +8,49 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-    # @articles = Article[:title]
-    # binding.pry
-    # erb :'index'
+    erb :'index'
   end
 
   get '/articles/new' do
-    erb :'new'
+    erb :new
   end
 
   post '/articles' do
-    @article = Article.new(params)
-    # redirect '/articles'
-    erb :'index'
+    Article.create(params)
+    redirect "/articles/#{Article.last.id}"
   end
 
   get '/articles' do
     @articles = Article.all
-    erb :'index'
+    erb :index
   end
 
   get '/articles/:id' do
     @article = Article.find(params["id"])
-    erb :'show'
+    erb :show
   end
 
   get '/articles/:id/edit' do
-    erb :'edit'
+    @article = Article.find(params["id"])
+    erb :edit
   end
 
   patch '/articles/:id' do
     id = params["id"]
     new_params = {}
-    old_article = Post.find(id)
+    old_article = Article.find(id)
     new_params[:title] = params["title"]
     new_params[:content] = params["content"]
-    old_post.update(new_params)
+    old_article.update(new_params)
 
     redirect "/articles/#{id}"
   end
 
-  delete '/articles/:id/delete' do
+  delete '/articles/:id' do
     @article = Article.find(params["id"])
-    @particle.destroy
+    @article.destroy
     erb :delete
+
+    redirect "/articles"
   end
 end
