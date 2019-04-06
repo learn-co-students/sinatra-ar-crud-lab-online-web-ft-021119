86c48cc0ab290eaf8370c94da31ce2e85269eebe
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-04-06 18:33:14 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,9 +9,10 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-    # erb :index
+    erb :index
   end
 
+##CREATE CRUD Actions
   get '/articles/new' do
     erb :new
   end
@@ -19,6 +20,26 @@ class ApplicationController < Sinatra::Base
   post '/articles' do
     Article.create(title: params[:title], content: params[:content])
     erb :index
+  end
+
+##READ CRUD Actions
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/:id' do
+    @article = Article.find_by(params[:id])
+    erb :show
+  end
+
+##UPDATE CRUD Actions
+  get '/articles/:id/edit'
+  erb :edit
+
+  patch '/articles/:id' do
+    @article = Article.find_by(params[:id])
+    #NEED to update and save revised Article object
   end
 
 end
