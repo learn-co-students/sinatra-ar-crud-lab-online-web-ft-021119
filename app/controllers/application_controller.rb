6f604190efd54949d5cc6328c3a5bd73120f01d1
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-04-08 17:27:13 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,37 +23,38 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/articles' do
-    # binding.pry
-    @article = Article.new(title: params[:title], content: params[:content])
-binding.pry
-    erb "/@articles/#{@article.id}"
-
+    @article = Article.create(title: params[:title], content: params[:content])
+    redirect to "/articles/#{@article.id}"
   end
 
 ##READ CRUD Actions
   get '/articles/:id' do
-    @article = Article.find_by(params[:id])
+    @article = Article.find_by({id: params[:id]})
+    # binding.pry
     erb :show
   end
 
 ##UPDATE CRUD Actions
   get '/articles/:id/edit' do
-    @article = Article.find_by(params[:id])
+    @article = Article.find_by(id: params[:id])
     erb :edit
+    # binding.pry
   end
 
 
   patch '/articles/:id' do
-    @article = Article.find_by(params[:id])
+    # binding.pry
+    @article = Article.find_by(id: params[:id])
+    # binding.pry
     @article.title = params[:title]
     @article.content = params[:content]
     @article.save
-    # @article.update(params[:article])
     redirect to "/articles/#{@article.id}"
   end
 
 ##DELETE CRUD Actions
-  delete '/articles/:id' do
+  delete '/articles/:id/delete' do
+    # binding.pry
     Article.destroy(params[:id])
     redirect to "/articles"
   end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,3 @@
 class Article < ActiveRecord::Base
-  attr_accessor :title, :content
-  # def initialize
-  # end
+  
 end
